7a3ee0c80c891c71409f2c5763306ca8d239443d
unknown <bb123eric@gmail.com>
2023-11-04 18:17:52 +0800

    update

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -53,7 +53,7 @@ kotlin {
 }
 
 group = "com.example.mysharedlib"
-version = "0.1.1"
+version = "0.1.2"
 
 val GITHUB_USER: String by project
 val GITHUB_TOKEN: String by project
--- shared/src/commonMain/kotlin/com/example/kmmlibexample/KtorExample.kt
+++ shared/src/commonMain/kotlin/com/example/kmmlibexample/KtorExample.kt
@@ -3,10 +3,22 @@ package com.example.kmmlibexample
 import com.example.kmmlibexample.model.Character
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.contentnegotiation.JsonContentTypeMatcher
+import io.ktor.client.request.accept
 import io.ktor.client.request.get
+import io.ktor.client.utils.EmptyContent.contentType
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.http.websocket.websocketServerAccept
+import io.ktor.serialization.kotlinx.json.json
 
 class KtorExample {
-    private val client = HttpClient()
+    private val client = HttpClient(){
+        install(ContentNegotiation) {
+            json()
+        }
+    }
 
     suspend fun getAllCharacters(): List<Character> {
         val response = client.get("https://rickandmortyapi.com/api/character")
